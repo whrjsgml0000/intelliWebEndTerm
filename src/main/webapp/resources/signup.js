b1bbc015638f6e16a4117f1963d775ee37758793
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-10 02:59:53 +0900

    feat : alert를 통해 오류 알리는 기능 추가

--- src/main/webapp/resources/signup.js
+++ src/main/webapp/resources/signup.js
@@ -12,15 +12,15 @@ const validate = () => {
   let signupForm = document.getElementById("signupForm");
 
   if (!id.value.match(/[a-zA-Z0-9]{6,12}/)) {
-    console.log(`id는 영어와 숫자로 6~12자리 구성이어야 합니다.`);
+    alert(`id는 영어와 숫자로 6~12자리 구성이어야 합니다.`);
     id.focus();
   } else if (!passwd.value.match(/[a-zA-Z0-9]{8,12}/)) {
-    console.log(`비밀번호는 영어와 숫자로 8~12자리 구성이어야 합니다.`);
+    alert(`비밀번호는 영어와 숫자로 8~12자리 구성이어야 합니다.`);
     passwd.focus();
     passwd.value = "";
     passwdCheck.value = "";
   } else if (passwd.value != passwdCheck.value) {
-    console.log(`비밀번호와 비밀번호확인의 입력 값이 다릅니다.`);
+    alert(`비밀번호와 비밀번호 확인의 입력 값이 다릅니다.`);
     passwd.focus();
     passwd.value = "";
     passwdCheck.value = "";
